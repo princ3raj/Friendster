8ba0c4408309d48573f4e1c9ddf0f755f783753e
princeraj <princ31999@gmail.com>
2020-05-13 15:13:27 +0530

    added 3 fragments for newsfeed, notification and friends and one profile activity

---
+++ app/src/main/java/com/example/friendster/Fragments/NotificationFragment.java
@@ -0,0 +1,31 @@
+package com.example.friendster.Fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.friendster.R;
+
+public class NotificationFragment extends Fragment {
+
+    private Context mContext;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        mContext=context;
+        super.onAttach(context);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       View view=inflater.inflate(R.layout.fragment_notification,container,false);
+       return  view;
+    }
+}
